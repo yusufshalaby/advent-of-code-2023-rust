5a319099f62ab602aa25198d30ee7e6deb9e5e4b
yusufshalaby <yusufshalaby@gmail.com>
2023-12-03 12:24:35 -0500

    renamed folders

---
+++ day01a/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!(
+        "{:?}",
+        include_str!("../input.txt")
+            .lines()
+            .map(|line| {
+                let mut first_digit: Option<u32> = None;
+                let mut last_digit: Option<u32> = None;
+                for c in line.chars().filter(|c| c.is_digit(10)) {
+                    if first_digit.is_none() {
+                        first_digit = Some(c.to_digit(10).unwrap());
+                    }
+                    last_digit = Some(c.to_digit(10).unwrap());
+                }
+                return first_digit.unwrap() * 10 + last_digit.unwrap();
+            })
+            .sum::<u32>()
+    );
+}
